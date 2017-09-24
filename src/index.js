9c3e6b216f98fc9ce3a59732071c0751d397eb62
Jay Baker <logikal@gmail.com>
2017-09-25 10:53:51 +1300

    fix(Joins): stop overwriting _joins metadata, assign instead

--- src/index.js
+++ src/index.js
@@ -44,6 +44,10 @@ module.exports = schema => {
         .filter(q => q !== {})
 
       this[name] = data
+
+      if (process.env.DEBUG) {
+        console.info(`[FACTORY] loaded ${name}`)
+      }
     }
 
     join(entity) {
@@ -53,43 +57,36 @@ module.exports = schema => {
       }
 
       const doJoin = (schema, join, on, prop) => {
+        if (process.env.DEBUG) {
+          console.info(`[FACTORY] joining ${name}.${prop} => ${join.schema}`)
+        }
+
         var data = _instances[schema][join.schema]
 
         this[name].forEach(item => {
-          // if (item[prop]._join) {
-          //   return
-          // }
-
           if (
             _joins.hasOwnProperty(name) &&
             _joins[name].hasOwnProperty(prop)
           ) {
+            // console.log(`${name}.${prop} =`, 'join exists - skipping')
             return
           }
 
           var joinData = data.filter(d => on(item, d))[0] || {}
 
           // add some meta data to the joins array
-          _joins[name] = {
+          _joins[name] = Object.assign({}, _joins[name], {
             [prop]: {
               schema: _schemas[schema].name,
               from: name,
-              originalValue: item[prop].id,
+              originalValue: joinData.id,
               property: prop,
-              id: item[prop].id
+              id: joinData.id
             }
-          }
-
-          // joinData._join = true
-          // joinData._schema = _schemas[schema].name
-          // joinData._value = item[prop]
-          // joinData._prop = prop
-          // item[`_${prop}`] = joinData
+          })
 
           Object.defineProperty(item, prop, {
             get: function() {
-              // return item[`_${prop}`]
-
               return (
                 data.filter(d => on({ [prop]: _joins[name][prop].id }, d))[0] ||
                 {}
